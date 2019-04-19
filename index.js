7d5c66554c3da145ca248de7d8b8be18ff3c9a64
Wayne.Xu <wayne.xu.work@gmail.com>
2019-04-18 21:19:20 -0400

    check added

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const ethers = require('ethers');
 
 function verifyMessage(message, signature) {
-    return ethers.utils.verifyMessage(message, signature);
+    if(message && signature) {
+        return ethers.utils.verifyMessage(message, signature);
+    } else {
+        return "variable can not be null";
+    }
 }
 
 module.exports = verifyMessage;
